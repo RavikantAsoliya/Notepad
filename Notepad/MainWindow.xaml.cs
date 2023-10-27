528b26f72e585d41342ddae7e7a9b54adbe8d190
Ravikant Asoliya <ravikant.asoliya@gmail.com>
2023-10-27 12:34:38 +0530

    Implement 'New' Command Functionality
    This commit introduces the 'New' command functionality, which allows users to create a new empty document. If the current document has unsaved changes, the user is prompted to save or discard them before proceeding. If the user chooses to save, the document is saved before creating the new one. If they cancel or encounter an error while saving, no new document is created. This feature enhances the user experience by ensuring data integrity and supporting the creation of fresh documents.

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notepad.Windows;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -46,9 +47,73 @@ namespace Notepad
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the execution of the "New" command, which creates a new empty document.
+        /// If there are unsaved changes in the current document, it prompts the user to save or discard them.
+        /// </summary>
         private void New_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            
+            // Check if there are unsaved changes in the current document.
+            if (ShouldSave)
+            {
+                // Prompt the user to save changes and get their choice (true for save, false for discard, null for cancel).
+                bool? result = AskToSaveFile();
+
+                if (result == true && File.Exists(FilePath))
+                {
+                    // Save the current document before creating a new one.
+                    // I will implement it later.
+                }
+                else if (result == null || (result == true ))//&& SaveAsNewDocument() == false
+                {
+                    // User canceled or encountered an error while saving, so do not create a new document.
+                    return;
+                }
+            }
+
+            // Create a new empty document.
+            CreateNewDocument();
+        }
+
+        /// <summary>
+        /// Opens a custom dialog to ask the user if they want to save changes to the current document.
+        /// </summary>
+        /// <returns>
+        ///   <para><c>true</c> if the user chooses to save changes,</para>
+        ///   <para><c>false</c> if the user chooses to discard changes,</para>
+        ///   <para><c>null</c> if the user cancels the operation.</para>
+        /// </returns>
+        private bool? AskToSaveFile()
+        {
+            // Create a new instance of the SaveMessageBox, a custom dialog for asking to save changes.
+            SaveMessageBox askToSave = new SaveMessageBox
+            {
+                // Set the message text based on whether a file path (FilePath) exists or not.
+                Message = File.Exists(FilePath)
+                    ? "Do you want to save changes to " + FilePath + "?" // File path exists, use it in the message.
+                    : "Do you want to save changes to " + FileName + "?", // File path doesn't exist, use the file name.
+                Owner = this // Set the owner of the dialog to this current window.
+            };
+
+            // Show the dialog to the user and Return the result of the user's choice (true for save, false for discard, null for cancel).
+            return askToSave.ShowDialog();
+        }
+
+        /// <summary>
+        /// Creates a new, empty document by clearing the text area and resetting document properties.
+        /// </summary>
+        private void CreateNewDocument()
+        {
+            // Clear the text area, removing any existing content.
+            TextArea.Clear();
+
+            // Set the document properties for a new, untitled document.
+            FileName = "Untitled";  // Set the default file name.
+            FilePath = FileData = "";  // Clear file-related data as it's a new document.
+
+            // Update the application title to reflect the new document state.
+            ShouldSave = false;  // No unsaved changes in the new document.
+            this.Title = FileName + " - Notepad";  // Update the window title.
         }
 
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -94,6 +159,27 @@ namespace Notepad
         private void TimeDate_Executed(object sender, ExecutedRoutedEventArgs e)
         {
 
+        }
+
+        private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Check if the current text matches the loaded file data
+            if (TextArea.Text == FileData)
+            {
+                // If the text matches, set the window title to the file name without an asterisk
+                this.Title = FileName + " - Notepad";
+
+                // Mark that there's no need to save changes
+                ShouldSave = false;
+            }
+            else
+            {
+                // If the text has changed, set the window title with an asterisk to indicate unsaved changes
+                this.Title = "*" + FileName + " - Notepad";
+
+                // Mark that there are unsaved changes
+                ShouldSave = true;
+            }
         }
     }
 }
